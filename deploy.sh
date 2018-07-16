f19f60b71545661410a433c5a44e90123b998acd
Brian Ketelsen <bketelsen@gmail.com>
2018-07-16 07:17:36 -0400

    add cute bouncy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-az container create -g bkdevbox --name playground --image bketelsen/playground:1 --cpu 1 --memory 1 --dns-name-label bjkplayground
+az container create -g bkdevbox --name playground --image bketelsen/playground:latest --cpu 1 --memory 1 --dns-name-label bjkplayground
